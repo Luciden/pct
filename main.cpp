8f814b2dc1accce926473e921ae43d6d1ea8bbcc
Dennis Merkus <dennis.merkus@gmail.com>
2014-01-23 00:20:13 +0100

    Hello World, and beyond!

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 22 Jan 2014
+ *      Author: Dennis
+ */
+#include <iostream>
+
+int main( int argc, char *argv[] ) {
+	std::cout << "Hello World!" << std::endl;
+
+	return 0;
+}
+
+
