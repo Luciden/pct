056c066dab65f2487d22aae7b22343ee19b875fd
Dennis Merkus <dennis.merkus@gmail.com>
2014-01-26 16:24:04 +0100

    Reorganized and added basic command parameter parsing.

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * @author Dennis Merkus
+ */
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+using std::string;
+
+int main( int argc, char *argv[] ) {
+	// read command line parameters and decide what to do
+	if( argc <= 1 ) {
+		cout << "Usage: pct {command} [options]" << endl
+			 << endl
+			 << "For further help, type 'pct help'" << endl;
+	}
+	else {
+		string command = argv[1];
+
+		if( command == "help" ) {
+			cout << "Possible commands: " << endl;
+		}
+		else {
+			cout << "Invalid command." << endl;
+		}
+	}
+
+	return 0;
+}
